60958847365fde500803dca47521c25620465896
victor <vpoten@gmail.com>
2019-04-02 22:47:31 +0200

    update 100genomes vcf download

--- scripts/bio_web_data/down_1000g_vcf.groovy
+++ scripts/bio_web_data/down_1000g_vcf.groovy
@@ -1,11 +1,12 @@
 #!/usr/bin/env groovy
 
-String _1000G_FTP = 'ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20110521/'
-String S3_VCF_FILE = 'ALL.{chr}.phase1_release_v3.20101123.snps_indels_svs.genotypes.vcf.gz'
-String S3_VCF_TBI = 'ALL.{chr}.phase1_release_v3.20101123.snps_indels_svs.genotypes.vcf.gz.tbi'
+String _1000G_FTP = 'ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/release/20130502/'
+String S3_VCF_FILE = 'ALL.{chr}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz'
+String S3_VCF_TBI = 'ALL.{chr}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz.tbi'
 String WGET_COMMAND = "wget -nv --no-proxy"
 
-def chrs = (1..22).collect{ 'chr'+it }
+// def chrs = (1..22).collect{ 'chr'+it }
+def chrs = [1, 3, 5, 7].collect{ 'chr'+it }
 chrs << 'chrX'
 
 String outdir = this.args[0]
